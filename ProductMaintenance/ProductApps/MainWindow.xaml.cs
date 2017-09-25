7927aa0effd9fa770ef34c09fec24bcf5448781e
yash <shelatkaryash@gmail.com>
2017-09-25 11:03:32 +0930

    Done with GstUI

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+
+
             }
             catch (FormatException)
             {
